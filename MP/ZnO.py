4a15c667e4f2f545db4dc20fe7b90c038dc32250
hkve <haakon.kvernmoen@gmail.com>
2021-07-19 10:12:25 +0200

    MP band structures

--- MP/ZnO.py
+++ MP/ZnO.py
@@ -1,20 +1,18 @@
 import sys, os
 from get_structure import load_structure
 from get_bands import get_bands, make_band_objects
+from plot_structure import plot_brillouin, plot_bandstructure
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from JDOS import JDOS
 from band import Band
 from plots import plot_bands, plot_bands_and_JDOS, plot_integrated_density, plot_waterfall
 
-def plot(run=False):
-	bs_ZnO = load_structure("data/ZnO.json")
-	bs_ZnO = bs_ZnO.apply_scissor(3)
-
-	k, v, c = get_bands(bs_ZnO, "\Gamma-A", n_val=5,n_con=3)
+def jdos(bs, run=False):
+	k, v, c = get_bands(bs, "K-\Gamma", n_val=5,n_con=3)
 	bands = make_band_objects(k,v,c, interpolate=True, n_points=500)
 
-	filename = "ZnO"
+	filename = "ZnO_KG"
 	jdos = JDOS()
 
 	if run:
@@ -32,4 +30,13 @@ def plot(run=False):
 	#Q, E, J = jdos.get_data()
 	#plot_bands_and_JDOS(Q, E, J, bands, JDOS_options={"smooth": 1})
 
-plot(run=False)
+
+
+if __name__ == "__main__":
+	bs_ZnO = load_structure("data/ZnO.json")
+	bs_ZnO = bs_ZnO.apply_scissor(3.2)
+	
+	jdos(bs_ZnO, run=True)
+
+	#plot_brillouin(bs_ZnO)
+	#plot_bandstructure(bs_ZnO, title="ZnO band structure")
--- MP/plot_structure.py
+++ MP/plot_structure.py
@@ -1,12 +1,25 @@
 from pymatgen.electronic_structure.plotter import BSPlotter
 from pymatgen.electronic_structure.bandstructure import BandStructureSymmLine
 from get_structure import load_structure
+import matplotlib.pyplot as plt
 
 def plot_brillouin(bs):
 	BSPlotter(bs).plot_brillouin()
 
-def plot_bandstructure(bs):
-	BSPlotter(bs).get_plot().show() 
+def plot_bandstructure(bs, title=""):
+	bsplot = BSPlotter(bs)
+	bsplot.get_plot()
+
+	ax = plt.gca()
+	ax.set_title(title, fontsize=24)
+	ax.set_xlabel("")
+	ax.set_ylabel("Energy (eV)", fontsize=24)
+
+	#ax.plot((), (), c="r", lw=4)
+
+	ax.get_legend().remove()
+
+	plt.show()
 
 if __name__ == "__main__":
 	#BandStructureSymmLine object: https://pymatgen.org/pymatgen.electronic_structure.bandstructure.html
@@ -15,6 +28,6 @@ if __name__ == "__main__":
 	#bs_SnO2 = load_structure("data/SnO2.json")
 	#bs_SnO2 = bs_SnO2.apply_scissor(new_band_gap=3) # Move CBM
 	#print(bs_ZnO.structure)
-	plot_brillouin(bs_ZnO)
-
+	
+	#plot_brillouin(bs_ZnO)
 	#plot_bandstructure(bs_SnO2)
